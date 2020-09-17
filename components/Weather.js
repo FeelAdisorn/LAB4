c0a46a0e0b1abb100f3ff456f89834012f5eeb4b
FeelAdisorn <s6135512033@phuket.psu.ac.th>
2020-09-17 11:57:10 +0700

    commit

--- components/Weather.js
+++ components/Weather.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, ImageBackground, StyleSheet } from 'react-native';
 import Forecast from './Forecast';
+const apiKey = 'f532d90ad5e247a1ed3cf64f0d7e0bc2' //Apikey
 
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
@@ -8,33 +9,23 @@ export default function Weather(props) {
         description: 'description',
         temp: 0,
     })
-    return (
-        <View>
-            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-                <View style={styles.cover}>
-                    <Text style={styles.medium}>Zip Code: {props.zipCode}</Text>
-                    <Forecast {...forecastInfo} />
-                </View>
-            </ImageBackground>
-        </View>
-    );
-}
-const styles = StyleSheet.create({
-    backdrop: {
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
-    },
-    cover: {
-        backgroundColor: '#000', 
-        width: '100%',
-        height: 300,
-        opacity: 0.5,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    medium: {
-        fontSize: 20, 
-        color: '#FFF',
-    }
-});
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {              
+   
+   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=${apiKey}`)
+                   .then((response) => response.json())
+                   .then((json) => {
+                       setForecastInfo({
+                           main: json.weather[0].main,
+                           description: json.weather[0].description,
+                           temp: json.main.temp
+                       });
+                   })
+                   .catch((error) => {
+                       console.warn(error);
+                   });
+           }
+       }, [props.zipCode])
+   
+};
